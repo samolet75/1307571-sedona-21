7b2d0337a662d734a85aaeaa532d62ac406c7198
Илья <samolet75@bk.ru>
2022-03-31 23:30:25 +0300

    qqq

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,10 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
+const htmnmin = require("gulp-htmlmin");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const gulpWebp = require("gulp-webp");
 const sync = require("browser-sync").create();
 
 // Styles
@@ -23,11 +25,19 @@ const styles = () => {
     ]))
     .pipe(sourcemap.write("."))
     .pipe(rename("styles.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+//HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+  .pipe(htmnmin({collapseWhitespace:true}))
+  .pipe(gulp.dest("dist"))
+}
 
 // Images
 const images = () => {
@@ -46,7 +56,7 @@ exports.images = images;
 const createWebp = () => {
 return gulp.src("source/img/**/*.{jpg,png}")
   .pipe(webp({qulity:90}))
-  .pipe(gulp.dest("source/img"))
+  .pipe(gulp.dest("dist/img"))
 }
 
 exports.createWebp = createWebp;
@@ -56,7 +66,7 @@ exports.createWebp = createWebp;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'dist'
     },
     cors: true,
     notify: false,
@@ -73,6 +83,17 @@ const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
+
+//Build
+const build = gulp.parallel(
+  styles,
+  html,
+  images,
+  createWebp
+)
+
+exports.build = build;
+
 
 exports.default = gulp.series(
   styles, server, watcher
